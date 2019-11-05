31509995a291b518450ff77d70a483b23ab390d7
VadimLofitsky <54962065+VadimLofitsky@users.noreply.github.com>
2019-11-05 04:40:09 +0300

    Created app runner class Application

--- RESTApp01/src/main/java/hello/Application.java
+++ RESTApp01/src/main/java/hello/Application.java
@@ -1,10 +1,11 @@
 package hello;
 
-import org.springframework.boot.Application;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringApplication
+@SpringBootApplication
 public class Application {
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
