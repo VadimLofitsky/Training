88fae4705681d677467b534acb59fb8d84fee636
VadimLofitsky <54962065+VadimLofitsky@users.noreply.github.com>
2019-11-13 00:31:13 +0300

    Added mapping method for simple testing of workability of app.

--- BookStoreExample/src/main/java/ru/lofitsky/training/book_store_example/controller/GreetingController.java
+++ BookStoreExample/src/main/java/ru/lofitsky/training/book_store_example/controller/GreetingController.java
@@ -4,9 +4,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+//TODO: think about renaming controller into smth like (Request/Response)Controller
 public class GreetingController {
     @RequestMapping("/")
     public String greeting() {
         return "Hello, world!";
     }
+
+    // for testing workability
+//    @RequestMapping("/book/{id}/{title}/{genre}/{publisher}")
+//    public Book greeting(@PathVariable(name="id") Integer id,
+//                         @PathVariable(name="title") String title,
+//                         @Nullable @PathVariable(name="genre") String genre,
+//                         @PathVariable(name="publisher") String publisher) {
+//
+//        return new Book(id, title, Genres.UNKNOWN, publisher);
+//    }
 }
