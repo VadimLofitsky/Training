240e4b26475a83c1ae6623ce047ae133859f9711
VadimLofitsky <54962065+VadimLofitsky@users.noreply.github.com>
2019-11-12 21:58:38 +0300

    Created .model package and enum of literature genres.

---
+++ BookStoreExample/src/main/java/ru/lofitsky/training/book_store_example/model/Genres.java
@@ -0,0 +1,11 @@
+package ru.lofitsky.training.book_store_example.model;
+
+public enum Genres {
+    SCIENCE_FICTION,
+    SATIRE,
+    DRAMA,
+    ACTION_AND_ADVENTURE,
+    ROMANCE,
+    MYSTERY,
+    HORROR
+}
