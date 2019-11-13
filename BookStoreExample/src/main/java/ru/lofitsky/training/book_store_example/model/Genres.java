ee4052c939f35bdbcce5cc67ba5d97adf3ecf402
VadimLofitsky <54962065+VadimLofitsky@users.noreply.github.com>
2019-11-13 14:10:11 +0300

    Added method for retreiving genre type by its string representation.

--- BookStoreExample/src/main/java/ru/lofitsky/training/book_store_example/model/Genres.java
+++ BookStoreExample/src/main/java/ru/lofitsky/training/book_store_example/model/Genres.java
@@ -1,5 +1,9 @@
 package ru.lofitsky.training.book_store_example.model;
 
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public enum Genres {
     UNKNOWN,                // for genres that are not listed below
     SCIENCE_FICTION,
@@ -8,5 +12,13 @@ public enum Genres {
     ACTION_AND_ADVENTURE,
     ROMANCE,
     MYSTERY,
-    HORROR
+    HORROR;
+
+    private final static Map<String, Genres> namesMap = Stream.of(Genres.values())
+            .collect(Collectors.toMap(stringName -> stringName.name().toUpperCase(), enumType -> enumType));
+
+    public static Genres getByString(String stringType) {
+        Genres result = namesMap.get(stringType.toUpperCase());
+        return (result != null) ? result : UNKNOWN;
+    }
 }
