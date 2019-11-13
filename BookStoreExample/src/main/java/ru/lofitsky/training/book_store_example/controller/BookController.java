fcb05c27c4b7d265130daa4afb1726f9d78c3379
VadimLofitsky <54962065+VadimLofitsky@users.noreply.github.com>
2019-11-13 14:19:52 +0300

    GreetingController renamed to BookController.

---
+++ BookStoreExample/src/main/java/ru/lofitsky/training/book_store_example/controller/BookController.java
@@ -0,0 +1,12 @@
+package ru.lofitsky.training.book_store_example.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BookController {
+    @RequestMapping("/")
+    public String greeting() {
+        return "Hello, world!";
+    }
+}
