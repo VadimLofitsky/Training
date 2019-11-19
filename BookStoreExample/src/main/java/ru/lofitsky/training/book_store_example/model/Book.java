b23d38db89c98ae9ceaf04cd2097879631ced5ee
VadimLofitsky <54962065+VadimLofitsky@users.noreply.github.com>
2019-11-19 23:05:32 +0300

    Added JPA annotations to Book Entity.

--- BookStoreExample/src/main/java/ru/lofitsky/training/book_store_example/model/Book.java
+++ BookStoreExample/src/main/java/ru/lofitsky/training/book_store_example/model/Book.java
@@ -3,11 +3,21 @@ package ru.lofitsky.training.book_store_example.model;
 import lombok.Data;
 import lombok.ToString;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
 @Data
 @ToString(includeFieldNames = true)
+@Entity
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private final long id;
+
+    @NotBlank(message = "Title is not optional")
     private final String title;
+
     private final Genres genre;
     private final String publisher;
 }
