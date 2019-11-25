ad5ae91dd478b3402bf89d234e95e86ff9b00eec
VadimLofitsky <54962065+VadimLofitsky@users.noreply.github.com>
2019-11-25 21:19:40 +0300

    Wrong tests execution fixed - import corrected.

--- BookStoreExample/src/test/java/ru/lofitsky/training/book_store_example/test/BookRepositoryIntegrationTest.java
+++ BookStoreExample/src/test/java/ru/lofitsky/training/book_store_example/test/BookRepositoryIntegrationTest.java
@@ -1,7 +1,7 @@
 package ru.lofitsky.training.book_store_example.test;
 
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
