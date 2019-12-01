bcd8b7c89b1f56d53c4a4e1bd924a054650dd912
VadimLofitsky <54962065+VadimLofitsky@users.noreply.github.com>
2019-12-01 16:28:03 +0300

    Genre input field replaced with select option.

--- BookStoreExample/src/main/java/ru/lofitsky/training/book_store_example/controller/BookController.java
+++ BookStoreExample/src/main/java/ru/lofitsky/training/book_store_example/controller/BookController.java
@@ -5,6 +5,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.lofitsky.training.book_store_example.model.Book;
+import ru.lofitsky.training.book_store_example.model.Genres;
+
+import java.util.Arrays;
 
 @Controller
 public class BookController {
@@ -16,7 +19,16 @@ public class BookController {
 
     @GetMapping("/addNewBook")
     public String addNewBook(Model model) {
-        model.addAttribute("book", new Book(""));
+        String[] genres = Arrays.stream(Genres.values()).map(Enum::name).toArray(String[]::new);
+
+        Book testBook = new Book("The Holy Bible");
+        testBook.setAuthor("Lord");
+        testBook.setGenre(Genres.MYSTERY);
+        testBook.setPublisher("the Time");
+
+        model.addAttribute("genres", genres);
+        model.addAttribute("book", testBook);
+
         return "addNewBook";
     }
 }
