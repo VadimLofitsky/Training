a8f1e376ffec95a2e5f9085a43036ada2837d9d5
VadimLofitsky <54962065+VadimLofitsky@users.noreply.github.com>
2019-12-01 14:21:24 +0300

    Changed controller annotation to provide correct request mapping.

--- BookStoreExample/src/main/java/ru/lofitsky/training/book_store_example/controller/BookController.java
+++ BookStoreExample/src/main/java/ru/lofitsky/training/book_store_example/controller/BookController.java
@@ -1,10 +1,12 @@
 package ru.lofitsky.training.book_store_example.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import ru.lofitsky.training.book_store_example.model.Book;
 
-@RestController
+@Controller
 public class BookController {
 
     @RequestMapping("/")
