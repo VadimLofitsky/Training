eec8862164d0b55f05fcf26fb835b0d19c77e4aa
VadimLofitsky <54962065+VadimLofitsky@users.noreply.github.com>
2019-12-01 10:04:51 +0300

    Added new GET endpoint 'addNewBook' which for now just returns a string.

--- BookStoreExample/src/main/java/ru/lofitsky/training/book_store_example/controller/BookController.java
+++ BookStoreExample/src/main/java/ru/lofitsky/training/book_store_example/controller/BookController.java
@@ -1,12 +1,19 @@
 package ru.lofitsky.training.book_store_example.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BookController {
+
     @RequestMapping("/")
     public String greeting() {
         return "Hello, world!";
+    }
+
+    @GetMapping("/addNewBook")
+    public String addNewBook() {
+        return "addNewBook";
     }
 }
