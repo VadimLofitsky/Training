c4544d3c6857afaa9cc7cd6195e71e3c118f08b8
VadimLofitsky <54962065+VadimLofitsky@users.noreply.github.com>
2019-12-04 01:08:24 +0300

    Changed parent class for more potential and flexibility.

--- BookStoreExample/src/main/java/ru/lofitsky/training/book_store_example/repository/BookRepository.java
+++ BookStoreExample/src/main/java/ru/lofitsky/training/book_store_example/repository/BookRepository.java
@@ -1,11 +1,11 @@
 package ru.lofitsky.training.book_store_example.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.lofitsky.training.book_store_example.model.Book;
 
 @Repository
-public interface BookRepository extends CrudRepository<Book, Long> {
+public interface BookRepository extends JpaRepository<Book, Long> {
 
     Book findByTitle(String title);
 }
