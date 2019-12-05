c4c320061b9c722e2c8a6a09ded2ee9e804ddd59
VadimLofitsky <54962065+VadimLofitsky@users.noreply.github.com>
2019-12-05 12:12:52 +0300

    Type of the 'genre' column changed to varchar.

--- BookStoreExample/src/main/java/ru/lofitsky/training/book_store_example/model/Book.java
+++ BookStoreExample/src/main/java/ru/lofitsky/training/book_store_example/model/Book.java
@@ -5,10 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Data
 @Builder
@@ -24,6 +21,9 @@ public class Book {
     private String title;
 
     private String author;
+
+    @Enumerated(EnumType.STRING)
     private Genres genre;
+
     private String publisher;
 }
